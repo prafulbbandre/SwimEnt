7950415845914d8326809a0d2657be2b6cd35f70
PRAFUL BANDRE <109658545+prafulbbandre@users.noreply.github.com>
2024-05-28 23:53:48 +0530

    added live reload feature

--- barcode.js
+++ barcode.js
@@ -1,2 +1,8 @@
-    let input = prompt("please input a number");
-    JsBarcode("#barcode1", input)
+let input = prompt("please input a number");
+// JsBarcode("#barcode1", input)
+var barcode = require('barcode');
+var code39 = barcode('code39', {
+    data: "it works",
+    width: 400,
+    height: 100,
+});
